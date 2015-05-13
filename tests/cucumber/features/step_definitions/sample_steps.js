989a192632046fd1d01c17e352f7fc1ff5ea9c2a
Sri Prasanna <k.sriprasanna@gmail.com>
2015-05-12 21:08:12 -0500

    updating packages

--- tests/cucumber/features/step_definitions/sample_steps.js
+++ tests/cucumber/features/step_definitions/sample_steps.js
@@ -9,23 +9,23 @@
 
     this.Given(/^I am a new user$/, function () {
       // no callbacks! DDP has been promisified so you can just return it
-      return this.ddp.callAsync('reset', []); // this.ddp is a connection to the mirror
+      return this.mirror.call('reset'); // this.ddp is a connection to the mirror
     });
 
     this.When(/^I navigate to "([^"]*)"$/, function (relativePath, callback) {
       // WebdriverIO supports Promises/A+ out the box, so you can return that too
       this.browser. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
-        url(url.resolve(process.env.HOST, relativePath)). // process.env.HOST always points to the mirror
+        url(url.resolve(process.env.ROOT_URL, relativePath)). // process.env.ROOT_URL always points to the mirror
         call(callback);
     });
 
     this.Then(/^I should see the title "([^"]*)"$/, function (expectedTitle, callback) {
       // you can use chai-as-promised in step definitions also
       this.browser.
-        waitForVisible('h1'). // WebdriverIO chain-able promise magic
+        waitForVisible('body *'). // WebdriverIO chain-able promise magic
         getTitle().should.become(expectedTitle).and.notify(callback);
     });
 
   };
 
-})();
+})(); 
